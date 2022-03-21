5e99464fa8d19e624660b86da8563c8525badc1b
Vanessa Mitelman <vanessamitelman@gmail.com>
2022-03-21 12:13:40 +0200

    fixed the map rating padding
    fixed the map rating padding so that it fits in properly

--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   paper: {
-    padding: '10px',
+    padding: '5px',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
